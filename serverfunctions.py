2b5fb1522233cd1c9edd622b39d93ec43819515e
Robert Chandler <robert.j.h.chandler@googlemail.com>
2016-06-21 16:07:37 +0100

    Started selector parser

---
+++ serverfunctions.py
@@ -0,0 +1,50 @@
+def parse_selector(selector, bulbs):
+	"""Takes selector and returns a list of ids that match, an empty array if valid selector but no match
+	 or None if invalid selector"""
+	ids = []
+
+	if selector == "all":
+		ids.append(bulbs.keys())
+
+	elif selector.startswith("label:"):
+		label = selector.split("label:", 1)[1]
+		for id, bulb in bulbs.iteritems():
+			if bulb.label == label:
+				ids.append(id)
+
+	elif selector.startswith("id:"):
+		ids.append(selector.split("id:",1)[1])
+
+	elif selector.startswith("group_id:"):
+		group_id = selector.split("group_id:", 1)[1]
+		for id, bulb in bulbs.iteritems():
+			if bulb.group.id == group_id:
+				ids.append(id)
+
+	elif selector.startswith("group:"):
+		group = selector.split("group:", 1)[1]
+		for id, bulb in bulbs.iteritems():
+			if bulb.group.name == group:
+				ids.append(id)
+
+	elif selector.startswith("location_id:"):
+		location_id = selector.split("location_id:", 1)[1]
+		for id, bulb in bulbs.iteritems():
+			if bulb.location.id == location_id:
+				ids.append(id)
+
+	elif selector.startswith("location:"):
+		location = selector.split("location:", 1)[1]
+		ids = []
+		for id, bulb in bulbs.iteritems():
+			if bulb.location.name == location:
+				ids.append(id)
+
+	elif selector.startswith("scene_id:"):
+		scene_id = selector.split("scene_id:", 1)[1]
+		#TODO
+		ids = None
+	else:
+		ids = None
+
+	return ids
